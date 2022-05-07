c875198abc07366c2f7bf03233298180ab9d5ea2
fran1999 <c.javierfranco99@gmail.com>
2022-05-07 01:25:26 -0300

    agregado de annotetions

--- src/main/java/ar/edu/unlp/info/bd2/model/SupportStaff.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/SupportStaff.java
@@ -1,10 +1,19 @@
 package ar.edu.unlp.info.bd2.model;
 import java.util.Collection;
+import javax.persistence.*;
+@Entity
+@Table ( name  = "supportStaff")
+@DiscriminatorValue("2")
 public class SupportStaff extends Personal{
+    @Column(name = "area")
     private String area;
+
+    public SupportStaff() {
+    }
 
     public SupportStaff(String nombreCompleto, String dni, Collection<Centre> centros, String area) {
         super(nombreCompleto, dni, centros);
         this.area = area;
     }
+
 }
