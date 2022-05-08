25b5e3da5fcd5312eeffcff2d3c1726b5f595101
Facundo Cadaa <fac081022@gmail.com>
2022-05-08 20:56:47 -0300

    Create Patient/Shot y save

--- src/main/java/ar/edu/unlp/info/bd2/repositories/VaxRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/VaxRepository.java
@@ -7,4 +7,14 @@ public class VaxRepository {
 
     @Autowired
     private SessionFactory sessionFactory;
+    
+    public void save(Object o) {
+    	try {
+    		this.sessionFactory.getCurrentSession().saveOrUpdate(o);
+    	}
+    	catch (Exception e) {
+    		this.sessionFactory.getCurrentSession().clear();
+    		throw e;
+    	}
+    }
 }
